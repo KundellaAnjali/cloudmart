6c52d20f797e78f3fce465f48463c85654f2ae96
Kundella Anjali (Omnicom) <kundella.anjali@omc.com>
2026-08-27 12:06:22 +0530

    authorization

---
+++ lambda/authorizer/index.py
@@ -0,0 +1,31 @@
+import boto3
+
+ssm = boto3.client('ssm')
+
+def handler(event, context):
+
+    token = event.get("authorizationToken", "")
+
+    response = ssm.get_parameter(
+        Name="/cloudmart/dev/auth/token",
+        WithDecryption=True
+    )
+
+    valid_token = response["Parameter"]["Value"]
+
+    if token == f"Bearer {valid_token}":
+        return {
+            "principalId": "cloudmart",
+            "policyDocument": {
+                "Version": "2012-10-17",
+                "Statement": [
+                    {
+                        "Action": "execute-api:Invoke",
+                        "Effect": "Allow",
+                        "Resource": "*"
+                    }
+                ]
+            }
+        }
+
+    raise Exception("Unauthorized")
